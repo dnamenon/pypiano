b8ebd4c097b904ba9217327b07f0a31929b5d4c6
Dev Menon <dev.n.menon@gmail.com>
2020-06-26 10:55:33 -0500

    add whitekeys, maing loop running

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import pygame
 from settings import Settings
 
 
-class AlienInvasion:
+class PyPiano:
     """overall class to manage game assets and behavior"""
 
     def __init__(self):
-        """initialize game """
+
         pygame.init()
 
         self.settings = Settings()
@@ -17,6 +17,29 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
-        self.screen.fill(self.settings.bg_color)
 
         pygame.display.set_caption("pyPiano")
+
+    def main_loop(self):
+
+        while True:
+
+            self._event_handler()
+
+            self._draw_screen()
+
+    def _draw_screen(self):
+        # redraw screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+
+        pygame.display.flip()
+
+    def _event_handler(self):
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+if __name__ == '__main__':
+    piano = PyPiano()
+    piano.main_loop()
