98ad06d4c774c958b68a0dce32aacc3a7f1a0ae9
Dev Menon <dev.n.menon@gmail.com>
2020-07-01 09:57:53 -0500

    added black keys

--- black_keys.py
+++ black_keys.py
@@ -9,13 +9,33 @@ class Black_Key:
 
         self.color = self.sets.black_key_color
 
-        self.rect = pygame.Rect(0,150, self.sets.whitekey_width,self.sets.whitekey_height)
-        self.border = pygame.Rect(0,150,self.sets.whitekey_width,self.sets.whitekey_height)
+        self.rect = pygame.Rect(0,0, self.sets.black_key_width,self.sets.black_key_height)
+        self.border = pygame.Rect(0,0,self.sets.black_key_width,self.sets.black_key_height)
         self.border_color = self.sets.key_border_color
 
-    def draw_key(self,x):
-        self.rect.x = 93+x
-        self.border.x = 93+x
+    def draw_key_1(self,x,c1, c2):
+        octave = 35*7
+        a = c2 % 2
+        if a == 0:
+             a = 2
+        self.rect.x = 93 + (2*a*x) + (octave*c1)
+        self.border.x = self.rect.x
+        self.rect.y = 150
+        self.border.y = self.rect.y
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.border_color, self.border, width=1)
 
-        pygame.draw.rect(self.screen,self.color,self.rect)
-        pygame.draw.rect(self.screen,self.border_color,self.border,width=1)
+
+
+    def draw_key_2(self, x, c1, c2):
+        octave = 35 * 7
+        a = c2 % 3
+        if a == 0:
+             a = 3
+        self.rect.x = 233 + (2 * a * x) + (octave * c1)
+        self.border.x = self.rect.x
+        self.rect.y =150
+        self.border.y = self.rect. y
+
+        pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.border_color, self.border, width=1)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from white_keys import White_Key
+from black_keys import Black_Key
 
 
 class PyPiano:
@@ -23,10 +24,11 @@ class PyPiano:
         pygame.display.set_caption("pyPiano")
 
 
-        self.white_keys = []
-        self._make_keys(self.white_keys)
+        self.keys = []
+        self._make_keys(self.keys)
+        print(self.keys)
         self.screen.fill(self.settings.bg_color)
-        self._draw_white_keys()
+        self._draw_keys()
         pygame.display.flip()
 
 
@@ -47,15 +49,53 @@ class PyPiano:
                 sys.exit()
 
     def _make_keys(self,listofkeys):
-        for value in range(0,29):
+
+        for value in range(1,30):
+
             listofkeys.append(White_Key(self))
 
-    def _draw_white_keys(self):
-        count = 0
 
-        for key in self.white_keys:
-            key.draw_key(count*35)
-            count +=1
+            if (value % 7 == 1 or value % 7 == 2 or value % 7 == 4 or value % 7 == 5 or value % 7 == 6) and value < 29:
+                #the extra c on top shouldn't have a black key assigned to it
+                listofkeys.append(Black_Key(self))
+
+
+    def _draw_keys(self):
+        count = 0
+        count2 = 1
+        count3 = 1
+
+
+        for key in self.keys:
+            if type(key) is White_Key:
+                key.draw_key((count)*35)
+                pygame.display.flip()
+                count +=1
+
+            elif count%7 <=3 and type(key) is Black_Key:
+
+                self._draw_black_keys_1(count,count2,key)
+
+                count2 +=1
+            elif count%7 > 3 and type(key) is Black_Key:
+
+                self._draw_black_keys_2(count,count3,key)
+
+                count3 += 1
+
+
+
+    def _draw_black_keys_1(self,num1,num2,key):
+        count1 = num1//7
+        print(count1)
+        key.draw_key_1(21,count1,num2)
+
+
+    def _draw_black_keys_2(self,num1,num2,key):
+        count1 = num1 // 7
+        print(num2)
+        key.draw_key_2(20, count1, num2)
+
 
 
 if __name__ == '__main__':
